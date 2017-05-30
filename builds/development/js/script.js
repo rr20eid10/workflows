3f146229331200054b04384817bade3756e85306
rr20eid10 <robin3athome@gmail.com>
2017-05-29 23:28:31 -0700

    moved template.js back to comp folder

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -86,7 +86,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('The most creative minds in Art');
+})('Creative minds in Art');
 
 fill;
 
@@ -98,7 +98,7 @@ fill;
 $(function() {
   var Mustache = require('mustache');
 
-  $.getJSON('../../js/data.json', function(data) {
+  $.getJSON('js/data.json', function(data) {
     var template = $('#speakerstpl').html();
     var html = Mustache.to_html(template, data);
     $('#speakers').html(html);    
